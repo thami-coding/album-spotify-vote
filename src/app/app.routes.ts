77f8d2eb0028a92c5fc14d4cc9f66aaed009ebbd
Thami <sainttsquared@gmail.com>
2025-12-23 19:20:33 +0200

    chore: updating build tasks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Albums } from './albums/albums';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'artists',
+    component: Home,
+    children: [{ path: 'new', component: Albums }],
+  },
+  {
+    path: '**',
+    redirectTo: 'artists',
+  },
+];
